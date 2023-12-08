d6af5224c0a0c52814012b47e092830e00ed4a8f
royalguard14 <abu.razia13@gmail.com>
2023-12-08 21:57:10 +0800

    hello

--- resources/views/frontweb/playlist.blade.php
+++ resources/views/frontweb/playlist.blade.php
@@ -6,16 +6,7 @@
     $(".facebook").addClass("is-active");
   };
 </script>
-<div class="main-header anim" style="--delay: 0s">Discover</div>
-<!-- top 2 fe-->
-<div class="main-blogs">
-  <div class="main-blog anim" style="--delay: .1s">
-   <div class="main-blog__title">Quran</div>
- </div>
- <div class="main-blog anim" style="--delay: .2s">
-   <div class="main-blog__title">Mosques & Center</div>
- </div>
-</div> <!-- end top 2 fe-->
+
 <!-- start of most watch -->
 <div class="small-header anim" style="--delay: .3s">Playlists</div>
 
@@ -29,7 +20,7 @@
  <div class="video anim" style="--delay: .<?php echo $x ?>s" onclick="window.location.href = '{{ route('playlist.videos', ['id' => $playlist->id]) }}'">
    <div class="video-time"></div>
    <div class="video-wrapper">
-    <img src="playlist.png" style="background-color: rgba(0, 0, 255, 0.5);" />
+   
     <div class="author-img__wrapper video-author">
      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check">
       <path d="M20 6L9 17l-5-5" />
